87b9782f54b2cb398965a440664a5eed69dec7db
Guilherme David Branco <branco@macpro.(none)>
2014-10-29 16:06:33 -0200

    Falta desempenho

--- monte_swift/monte_carlo.swift
+++ monte_swift/monte_carlo.swift
@@ -2,23 +2,23 @@ type file;
 
 app (file ret) reduce(file inps[], int _n_iter)
 {
-	_reduce _n_iter @filenames(inps) stdout=@filename(ret);
+	reduce_c _n_iter @filenames(inps) stdout=@filename(ret);
 }
 
-app (file out) call(int _iter, int i)
+app (file out) random_gen(int _iter, int seed)
 {
-	random_gen _iter i stdout = @filename(out);
+	random_gen_c _iter seed stdout = @filename(out);
 }
 
-int tot_per_proc = @toInt(@arg("tot_per_proc", "1000"));
-int procs = @toInt(@arg("procs", "3"));
+int tot = @toInt(@arg("tot", "1000"));
+int procs = @toInt(@arg("procs", "4"));
 
-tracef("tot_per_proc=%i procs=%i\n",tot_per_proc,procs);
+tracef("tot_per_proc=%i procs=%i\n",tot,procs);
 file outs[];
 foreach i in [0:procs-1]{
 	file outfile <single_file_mapper; file=@strcat("sim_",i,".out")>;
-	outfile = call(tot_per_proc, i);
+	outfile = random_gen(tot%/procs, i);
 	outs[i] = outfile;
 }
 file result <"result.txt">; 
-result = reduce(outs, procs*tot_per_proc);
+result = reduce(outs, tot);
